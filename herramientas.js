cf7884b1e2335bfe0dca3d7b70388f360e1b8d5d
santidalmasso <santidalmassoj@gmail.com>
2023-04-02 20:19:45 -0300

    mis herramientas

---
+++ herramientas.js
@@ -0,0 +1,7 @@
+function calcularRiesgo(programador) {
+  // Función para calcular el riesgo percibido por el asesino
+}
+
+function CalcularImpacto(programador) {
+  // Función para calcular el impacto en las noticias
+}
